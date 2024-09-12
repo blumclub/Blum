2fd95d59e5ec7020630776e0f3e03efd3ffe6988
Gonzalo <gonzalotorresgrau@gmail.com>
2024-09-12 08:57:08 -0300

    nos

--- src/Componentes/Nosotros/Nosotros.jsx
+++ src/Componentes/Nosotros/Nosotros.jsx
@@ -4,7 +4,8 @@ export default function Nosotros() {
 
   return (
     <section id="nosotros" className="relative z-10 bg-primary"> 
-      <article className="p-4 max-w-[780px] mx-auto mb-8 pb-8 md:pb-24 xl:pb-36  z-10">
+      <article className="p-4 max-w-[780px] mx-auto pb-16 md:pb-24 xl:pb-36 ">
+
         <h1 className="text-secondary text-3xl m-4 items-center text-center font-roboto">¡Bienvenidos al Primer Club Asociación Civil sin fines de lucro de Cannabis Medicinal en la localidad de Bernal, Buenos Aires, Argentina!</h1>
         <br />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
@@ -20,8 +21,8 @@ export default function Nosotros() {
       </article>
       
       {/* Curva ondulada SVG */}
-      <svg viewBox="0 0 1440 320" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute bottom-[-32px] md:bottom-[-35px] left-0 w-full h-auto">
-        <path d="M0,64L60,80C120,96,240,128,360,160C480,192,600,224,720,213.3C840,203,960,149,1080,128C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z" fill="#E99006" />
+      <svg viewBox="0 0 1440 160" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute bottom-[-1px] sm:bottom-0 lg:bottom-[-4px] left-0 w-full h-auto z-0">
+        <path d="M0,64L60,72C120,80,240,96,360,112C480,128,600,144,720,138.7C840,133,960,107,1080,96C1200,85,1320,93,1380,96L1440,100L1440,160L1380,160C1320,160,1200,160,1080,160C960,160,840,160,720,160C600,160,480,160,360,160C240,160,120,160,60,160L0,160Z" fill="#E99006" />
       </svg>
     </section>
   );
