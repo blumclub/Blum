585bde7435a3ba9d64b2e382c8b2ad21b3ebddd7
Gonzalo <gonzalotorresgrau@gmail.com>
2024-12-17 11:56:42 -0300

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,11 +3,7 @@ import Banner from '../Componentes/Banner/Banner';
 import Nosotros from '../Componentes/Nosotros/Nosotros';
 import Gallery from '../Componentes/Galeria/Galeria';
 import Reprocan from '../Componentes/Reprocan/Reprocan';
-<<<<<<< HEAD
 import PostInstagram from '../Componentes/PostInstagram/PostInstagram';
-=======
-// import PostInstagram from '../Componentes/PostInstagram/PostInstagram';
->>>>>>> 5351c4b20adf37345608a7db2ee748f6ab944655
 import Consultorio from '../Componentes/Consultorio/Consultorio';
 import Hidroponico from '../Componentes/Hidroponico/Hidroponico';
 import Catalogo from '../Componentes/Catalogo/Catalogo';
